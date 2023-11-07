e9e1aa916965bb3525d8509f43bfbab6fd3b85ef
Francisco Gleidson Teixeira <03538064393@DIGITALCOLLEGE.LOCAL>
2023-11-07 10:02:34 -0300

    update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 import 'boxicons';
@@ -7,6 +7,8 @@ import logo from "./assets/logo.png";
 
 const Header = () => {
     const { isLogged } = useContext(AuthContext);
+    const [cartOpen, setCartOpen] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
     return (
         <header>
             <div className="logo">
@@ -19,9 +21,33 @@ const Header = () => {
                 <box-icon name="search"></box-icon>
             </div>
             <div className="acoes">
-                <Link to={'/cadastro'}>Cadastre-se</Link>
-                <Link to={'/login'}>Entrar</Link>
-                <box-icon name="cart"></box-icon>
+                <Link to={'/cadastro'} className="underlined">Cadastre-se</Link>
+                <Link to={'/login'} className="filled">Entrar</Link>
+                <box-icon name="cart" items="0" onClick={() => setCartOpen(!cartOpen)}></box-icon>
+                <div className={`cart ${cartOpen && 'active'}`}>
+                    <h3>Meu Carrinho</h3>
+                    <ul>
+                        <li>
+                            <img src="" alt="" />
+                            <div className="info">
+                                <h3>Tênis Nike Revolution 6 Next Nature Masculino</h3>
+                                <h4>R$ 219,00 <del>R$ 219,00</del></h4>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="" alt="" />
+                            <div className="info">
+                                <h3>Tênis Nike Revolution 6 Next Nature Masculino</h3>
+                                <h4>R$ 219,00 <del>R$ 219,00</del></h4>
+                            </div>
+                        </li>
+                    </ul>
+                    <h3>Valor total: <span>R$ 219,00</span></h3>
+                    <div className="acoes">
+                        <Link className="underlined">Esvaziar</Link>
+                        <Link className="filled">Ver Carrinho</Link>
+                    </div>
+                </div>
             </div>
             <nav>
                 <ul>
